76d66d01482376e4d5c216c4c39ee3dc400fc0d5
janarhdan <janardhan.talari@gmail.com>
2020-04-14 20:27:32 -0500

    Test files

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
 import React, { Component } from "react";
-import logo from "./logo.svg";
 import "./App.css";
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      counter: 1,
+    };
+  }
   render() {
-    return <div data-test="component-app"></div>;
+    return <div></div>;
   }
 }
 
